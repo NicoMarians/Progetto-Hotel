39ea4b982987558f170e22c7d351be6f3bd1c247
nicoMarians <marianinicolo@itis-molinari.eu>
2024-10-15 11:46:30 +0200

    Creazione Download-Upload

--- Scripts/cache.js
+++ Scripts/cache.js
@@ -1,1 +1,46 @@
 key = "c6423649-0f5c-40c4-9bba-ee114eba20af";
+url = "web.progettimolinari.it/";
+
+function upload(valore) {
+    console.log("Upload",valore)
+    return new Promise((resolve,reject) => {
+        try{
+            fetch(url + 'set', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                    key: 'c6423649-0f5c-40c4-9bba-ee114eba20af',
+                },
+                body: JSON.stringify({
+                    key: 'progettoHotel',
+                    value: valore,  
+                }),
+            }).then((response) => response.json())
+            .then((data) => resolve());
+        } catch {
+            reject();
+        }
+    });
+  }
+  
+  function download() {
+    return new Promise((resolve,reject) => {
+        try{
+            fetch(url + 'get', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                    key: 'c6423649-0f5c-40c4-9bba-ee114eba20af',
+                },
+                body: JSON.stringify({
+                    key: 'seriea',
+                }),
+            }).then((response) => response.json()).
+            then((r) => resolve(r.result));
+        } catch {
+            reject()
+        }
+
+  });
+        
+  }
