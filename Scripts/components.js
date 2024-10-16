554d3f9fef0aff245c58ca2edca1e7c0e92d0ad4
Marco <ceriolimarco@itis-molinari.eu>
2024-10-16 18:28:42 +0200

    modifica components form

--- Scripts/components.js
+++ Scripts/components.js
@@ -1,2 +1,9 @@
 const divForm=document.getElementById("form");
 const divTable=document.getElementById("table");
+const createForm(elemento) =>{
+    let inputElement;
+    let callback=null;
+    return{
+
+    }
+}
