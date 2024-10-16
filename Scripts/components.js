9fa5d4f3be187a97c9400fa4628de380c32661e6
Tubos <loureiromanuel@itis-molinari.eu>
2024-10-16 19:56:36 +0200

    spostata la table

--- Scripts/components.js
+++ Scripts/components.js
@@ -24,3 +24,27 @@ const form=createForm(divForm);
 form.onsubmit(console.log);
 form.render();
 //La tebellla avra' una lista del genere: [["data","singola","doppia","suite"],[...]]
+
+const createTable = (parentElement) => {
+    let data = [];
+
+    return {
+        build: (dataInput) => {
+            data = dataInput;
+        },
+
+        render: (data) => {
+            //[["stanza","posti"]]
+            let htmlTable = '<table>';
+            htmlTable += data.map((row) => 
+                '<tr>' + row.map((col) => 
+                    '<td>' + col + '</td>').join('')+'</tr>').join('');
+            htmlTable += '</table>';
+            parentElement.innerHTMl = htmlTable
+        },
+    };
+};
+
+const table = createTable(document.querySelector('#table'));
+table.build([['Data', 'Singola', 'Doppia', 'Suite']]);
+table.render();
--- Scripts/script.js
+++ Scripts/script.js
@@ -10,27 +10,3 @@ setInterval(() => {
     download();
     form.render();
 },1000);
-
-const createTable = (parentElement) => {
-    let data = [];
-
-    return {
-        build: (dataInput) => {
-            data = dataInput;
-        },
-
-        render: (data) => {
-            //[["stanza","posti"]]
-            let htmlTable = '<table>';
-            htmlTable += data.map((row) => 
-                '<tr>' + row.map((col) => 
-                    '<td>' + col + '</td>').join('')+'</tr>').join('');
-            htmlTable += '</table>';
-            parentElement.innerHTMl = htmlTable
-        },
-    };
-};
-
-const table = createTable(document.querySelector('#table'));
-table.build([['Data', 'Singola', 'Doppia', 'Suite']]);
-table.render();
