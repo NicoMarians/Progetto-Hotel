c81553b51d100db728e6695e5908e8cebaebcbb1
Marco <ceriolimarco@itis-molinari.eu>
2024-10-16 19:04:54 +0200

    altre modifiche alla form

--- Scripts/components.js
+++ Scripts/components.js
@@ -1,6 +1,6 @@
 const divForm=document.getElementById("form");
 const divTable=document.getElementById("table");
-const createForm(elemento) =>{
+const createForm=(elemento) =>{
     let arrayElement;
     let callback=null;
     return{
@@ -8,6 +8,9 @@ const createForm(elemento) =>{
         onsubmit: (f) =>{callback=f;},
         render: () =>{
             elemento.innerHTML=arrayElement.map((line)=>{`<div>`+ line[0] + `<input id="` + line[0] + `" type="` + line[1] + `"></input></div>`}).join();
+            elemento.innerHTML=elemento.innerHTML+`<div><button type="button" id="send">invia</button></div>`
+            const button=documen.getElementById("button");
+            button.onclick
         },
     }
 }
