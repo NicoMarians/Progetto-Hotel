d273f622f087a95509250d1daf48e882d9d32c19
nicoMarians <marianinicolo@itis-molinari.eu>
2024-10-16 18:53:34 +0200

    setTimeout

--- Scripts/components.js
+++ Scripts/components.js
@@ -1,2 +1,4 @@
 const divForm=document.getElementById("form");
 const divTable=document.getElementById("table");
+
+//La tebellla avra' una lista del genere: [["data","singola","doppia","suite"],[data d]]
