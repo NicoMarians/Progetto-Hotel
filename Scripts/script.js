b620a22856e74322c5829e7e5220d0339ab0ee30
nicoMarians <marianinicolo@itis-molinari.eu>
2024-10-16 19:33:46 +0200

    checkRoomAvaliability

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,12 @@
 
 
-setInterval(download,1000);
+const checkRoomAvalibility = (singola,doppia,suite) => {
+    if (singola > rooms.singola) {alert("Non ci sono abbastanza camere singole in questa data");error = true;}
+    if (doppia > rooms.doppia) {alert("Non ci sono abbastanza camere singole in questa data");error = true;}
+    if (suite > rooms.suite) {alert("Non ci sono abbastanza camere singole in questa data");error = true;}
+}
+
+setInterval(() => {
+    download();
+    form.render();
+},1000);
